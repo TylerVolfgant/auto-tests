bb78a6a591d85ef20c8a98555b2bcc7ccae27937
tyler <volfgant1987@gmail.com>
2022-08-16 17:19:37 +0300

    multi browser

--- book-address/src/test/java/ru/stqa/prf/bookaddress/appmanager/ApplicationManager.java
+++ book-address/src/test/java/ru/stqa/prf/bookaddress/appmanager/ApplicationManager.java
@@ -3,19 +3,23 @@ package ru.stqa.prf.bookaddress.appmanager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
-    // private WebDriver wb;
-    public static String browser = "firefox";
+    private final String browser;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     public static WebDriver wb;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public static void stop() {
         wb.quit();
     }
-
     public void init() {
         if (browser.equals("firefox")) {
           WebDriverManager.firefoxdriver().setup();
@@ -23,6 +27,9 @@ public class ApplicationManager {
         } else if (browser.equals("chrome")) {
           WebDriverManager.chromedriver().setup();
           wb = new ChromeDriver();
+        }else if (browser.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            wb = new EdgeDriver();
         }
         wb.get("http://localhost:8080/addressbook/");
         groupHelper = new GroupHelper(wb);
--- book-address/src/test/java/ru/stqa/prf/bookaddress/tests/TestBase.java
+++ book-address/src/test/java/ru/stqa/prf/bookaddress/tests/TestBase.java
@@ -6,16 +6,10 @@ import ru.stqa.prf.bookaddress.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager("firefox");
 
     @BeforeMethod(alwaysRun = true)
-  //  public void setUp() throws Exception {
-  //    WebDriverManager.firefoxdriver();
-  //    wb = new FirefoxDriver();
-  //    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  //    wb.get("http://localhost:8080/addressbook/");
-  //    login("admin", "secret");
-  //  }
+
     public void setUp() throws Exception {
         app.init();
     }
