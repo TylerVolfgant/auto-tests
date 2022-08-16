e68fe458a74e0d3a3ed3a9cf3a36398923820a97
tyler <volfgant1987@gmail.com>
2022-08-16 21:58:57 +0300

    add checking and fulfilling of preconditions

--- book-address/src/test/java/ru/stqa/prf/bookaddress/appmanager/GroupHelper.java
+++ book-address/src/test/java/ru/stqa/prf/bookaddress/appmanager/GroupHelper.java
@@ -44,4 +44,15 @@ public class GroupHelper extends HelperBase{
     public void submitGroupModificaion() {
         Click(By.name("update"));
     }
+
+    public boolean isThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
+    }
+
+    public void createGroup(GroupData group) {
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+    }
 }
