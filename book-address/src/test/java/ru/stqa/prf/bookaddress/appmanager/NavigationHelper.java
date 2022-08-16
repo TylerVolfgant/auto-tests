9acae18b48e555a70725070f594f98549f3b93c7
tyler <volfgant1987@gmail.com>
2022-08-16 21:37:27 +0300

    add optimization between pages

--- book-address/src/test/java/ru/stqa/prf/bookaddress/appmanager/NavigationHelper.java
+++ book-address/src/test/java/ru/stqa/prf/bookaddress/appmanager/NavigationHelper.java
@@ -4,18 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase{
-    private WebDriver wb;
 
     public NavigationHelper(WebDriver wb) {
         super(wb);
     }
 
     public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wb.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
         Click(By.linkText("groups"));
       //wb.findElement(By.linkText("groups")).click();
     }
 
     public void gotoHomePage() {
+        if(isElementPresent(By.id("maintable"))){
+            return;
+        }
         Click(By.linkText("home"));
     }
 }
