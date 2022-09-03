0d3b94496116321ef830aa48a3f8c5cba3e3e485
tyler <volfgant1987@gmail.com>
2022-09-03 17:09:42 +0300

    add work with list

--- book-address/src/test/java/ru/stqa/prf/bookaddress/tests/GroupCreationTests.java
+++ book-address/src/test/java/ru/stqa/prf/bookaddress/tests/GroupCreationTests.java
@@ -6,18 +6,28 @@ import org.testng.annotations.*;
 import ru.stqa.prf.bookaddress.model.GroupData;
 import ru.stqa.prf.bookaddress.model.Groups;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-
-    @Test
-  public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups(){
+      List<Object[]> list = new ArrayList<Object[]>();
+      list.add(new Object[]{new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+      list.add(new Object[]{new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+      list.add(new Object[]{new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+      return list.iterator();
+    }
+    @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().gotoGroupPage();
     Groups before = app.Group().All();
-    GroupData group = new GroupData().withName("test2");
+    //GroupData group = new GroupData().withName("test2");
     app.Group().create(group);
     Groups after = app.Group().All();
     assertThat(after.size(), equalTo(before.size() + 1));
